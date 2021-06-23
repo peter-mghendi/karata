b53278b51a2baf5dabe4a369d80819c8486acfbe
sixpeteunder <plenjo15@gmail.com>
2021-06-23 22:01:03 +0300

    Add tiny cli program for testing

---
+++ src/Karata.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Karata.Cards;
+using Karata.Cards.Shufflers;
+
+var deck = Deck.GetStandardDeck();
+deck.Shuffle(ShuffleAlgorithm.FisherYates); // This doesn't work
+// deck.Shuffle(ShuffleAlgorithm.OrderByRandom); // This works
+DisplayDeck(deck: deck);
+
+void DisplayDeck(Deck deck)
+{
+    var cards = deck.Cards;
+    var title = $"| {"Card",-20}| {"Rank",-5}|";
+
+    Console.WriteLine($"{cards.Count} cards.\n");
+    Console.WriteLine($"{title}\n{new string('-', title.Length)}");
+    foreach (var card in cards) Console.WriteLine($"| {card.Name,-20}| {card.Rank,-5}|");
+}
