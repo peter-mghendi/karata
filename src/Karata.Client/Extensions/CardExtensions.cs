2b4e2e580f359eeeb311025d5d66995d0afe6538
sixpeteunder <plenjo15@gmail.com>
2022-12-22 18:23:30 +0300

    Refactor ImageSrc into an extension method on Card

---
+++ src/Karata.Client/Extensions/CardExtensions.cs
@@ -0,0 +1,16 @@
+using Karata.Cards;
+using static Karata.Cards.Card.CardFace;
+
+namespace Karata.Client.Extensions;
+
+public static class CardExtensions
+{
+    public static string GetImageSrc(this Card card, bool faceUp = true)
+    {
+        if (!faceUp) return "img/cards/Back.svg";
+        
+        return card is { Face: Joker }
+            ? $"img/cards/{card.Suit.ToString()}.svg"
+            : $"img/cards/{card.Face.ToString()}{card.Suit.ToString()}.svg";
+    }
+}
