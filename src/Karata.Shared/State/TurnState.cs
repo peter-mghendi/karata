e088ef878177ca57313fd2c4650ef770bc2b124e
peter-mghendi <plenjo15@gmail.com>
2025-08-05 10:56:09 +0300

    Create CompositeStateAction

--- src/Karata.Shared/State/TurnState.cs
+++ src/Karata.Shared/State/TurnState.cs
@@ -9,7 +9,7 @@ namespace Karata.Shared.State;
 public class TurnState(ImmutableList<Card> turn, ImmutableArray<Interceptor<ImmutableList<Card>>> interceptors)
     : Store<ImmutableList<Card>>(turn, interceptors)
 {
-    public record Add((Card Card, int Position) Info) : StateAction<ImmutableList<Card>>
+    public record Insert((Card Card, int Position) Info) : StateAction<ImmutableList<Card>>
     {
         public override ImmutableList<Card> Apply(ImmutableList<Card> state) => state.Insert(Info.Position, Info.Card);
     }
@@ -24,9 +24,6 @@ public class TurnState(ImmutableList<Card> turn, ImmutableArray<Interceptor<Immu
         public override ImmutableList<Card> Apply(ImmutableList<Card> state) => state.Remove(Card);
     }
 
-    // TODO: CompositeAction
-    public record Reorder((Card Card, int Position) Info) : StateAction<ImmutableList<Card>>
-    {
-        public override ImmutableList<Card> Apply(ImmutableList<Card> state) => state.Remove(Info.Card).Insert(Info.Position, Info.Card);
-    }
+    public record Reorder((Card Card, int Position) Info)
+        : CompositeStateAction<ImmutableList<Card>>([new Remove(Info.Card), new Insert(Info)]);
 }
