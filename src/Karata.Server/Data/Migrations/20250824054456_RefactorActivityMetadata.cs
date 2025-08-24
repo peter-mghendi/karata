4846975e907dec8e62cf7fbd5e1cd627c73134d9
peter-mghendi <plenjo15@gmail.com>
2025-08-24 08:50:40 +0300

    Refactor activity metadata

---
+++ src/Karata.Server/Data/Migrations/20250824054456_RefactorActivityMetadata.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Karata.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorActivityMetadata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Metadata",
+                table: "Activities",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "jsonb");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Metadata",
+                table: "Activities",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
