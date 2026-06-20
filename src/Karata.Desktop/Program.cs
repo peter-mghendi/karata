f7f8d7d61530bf2c5f47d252d5e0fbfb4bd9a29c
peter-mghendi <plenjo15@gmail.com>
2026-06-20 08:07:00 +0300

    Set up Photino desktop project

---
+++ src/Karata.Desktop/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Photino.Blazor;
+
+namespace Karata.Desktop;
+
+//NOTE: To hide the console window, go to the project properties and change the Output Type to Windows Application.
+// Or edit the .csproj file and change the <OutputType> tag from "WinExe" to "Exe".
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        var builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        builder.Services.AddLogging();
+        builder.RootComponents.Add<App>("app");
+   
+        var app = builder.Build();
+
+        // customize window
+        app.MainWindow
+            .SetIconFile("favicon.ico")
+            .SetTitle("Karata Desktop");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
