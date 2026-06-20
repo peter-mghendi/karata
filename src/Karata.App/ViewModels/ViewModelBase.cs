c896169b6e1df046f7911382752dcd6162b797af
peter-mghendi <plenjo15@gmail.com>
2026-06-20 05:33:00 +0300

    Delete Avalonia project

--- src/Karata.App/ViewModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using ReactiveUI;
-
-namespace Karata.App.ViewModels;
-
-public abstract class ViewModelBase : ReactiveObject, IRoutableViewModel
-{
-    public abstract string? UrlPathSegment { get; }
-    public abstract IScreen HostScreen { get; }
-}
