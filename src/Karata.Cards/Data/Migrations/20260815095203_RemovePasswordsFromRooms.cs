9c06f803ea910b045905054b0493c776291c1329
peter-mghendi <plenjo15@gmail.com>
2026-08-15 12:52:47 +0300

    Remove passwords from room schema

---
+++ src/Karata.Cards/Data/Migrations/20260815095203_RemovePasswordsFromRooms.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Karata.Cards.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovePasswordsFromRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Hash",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Rooms");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Hash",
+                table: "Rooms",
+                type: "bytea",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Salt",
+                table: "Rooms",
+                type: "bytea",
+                nullable: true);
+        }
+    }
+}
